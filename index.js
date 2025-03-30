a961e2cc4ed5fb7f65ce6966fe3bd354c5dd44f1
Romulo Reis <romulosistemas@hotmail.com>
2025-03-30 16:02:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  const msg = req.body.message || "Mensagem não recebida.";
+  res.send({ resposta: "Você disse: " + msg });
+});
+
+app.listen(PORT, () => {
+  console.log('Servidor Node.js rodando na porta', PORT);
+});
